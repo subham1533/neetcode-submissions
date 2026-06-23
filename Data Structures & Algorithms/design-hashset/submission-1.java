55017eb0ac0d3017acbf71b3322c3120c0d92ba1
Subham Tomar <subhamtomar98@gmail.com>
2026-06-23 23:24:51 +0530

    Add: design-hashset - submission-1

---
+++ Data Structures & Algorithms/design-hashset/submission-1.java
@@ -0,0 +1,21 @@
+public class MyHashSet {
+    private List<Integer> data;
+
+    public MyHashSet() {
+        data = new ArrayList<>();
+    }
+
+    public void add(int key) {
+        if (!data.contains(key)) {
+            data.add(key);
+        }
+    }
+
+    public void remove(int key) {
+        data.remove(Integer.valueOf(key));
+    }
+
+    public boolean contains(int key) {
+        return data.contains(key);
+    }
+}
